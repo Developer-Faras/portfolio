e5aeb0f6dae2c858e27b6911cc066dc02dfaaa6a
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-06-26 16:11:06 +0600

    Complete Portfolio Design

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// // Portfolio After Alignment
+// const portfolioItem = document.querySelectorAll('.portfolio-item');
+// for (let i = 0; i < portfolioItem.length; i++) {
+//     const element = portfolioItem[i];
+
+//     const after = element.querySelector('.after');
+
+//     element.addEventListener('mousemove', (e) => {
+//         let x = e.clientX;
+//         let y = e.clientY;
+
+//         let top = toString(x);
+    
+//         after.style.top = '200px';
+        
+//         console.log(top);
+//     })
+// }
+
+
+// console.log('loaded');
